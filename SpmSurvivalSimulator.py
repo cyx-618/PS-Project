083eba0830ede36cb91c0a4df2aca6696e9eaac7
cyx-618 <leewanxuan88@gmail.com>
2025-12-12 20:31:24 +0800

    Add files via upload

---
+++ SpmSurvivalSimulator.py
@@ -0,0 +1,9 @@
+#***************************************************************************
+#Course:CSP1114 PROBLEM SOLVING AND PROGRAM DESIGN
+#LECTURE / LAB SECTION: TC2L/TL8L
+#Trimester:2530
+#Group Name: TL8L-14
+#Names: CHEONG YI XUAN | KWONG JIA QI
+#IDs: 252FC25349 | 252FC253R0
+#***************************************************************************
+
