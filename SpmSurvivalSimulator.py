a3f9837801b67ef7bc9e0b518508360226d7ac6c
rachel <leewanxuan88@gmail.com>
2025-12-16 08:04:48 +0800

    Update file

--- SpmSurvivalSimulator.py
+++ SpmSurvivalSimulator.py
@@ -7,3 +7,55 @@
 #IDs: 252FC25349 | 252FC253R0
 #***************************************************************************
 
+import termcolor
+import pyfiglet
+title=pyfiglet.figlet_format("SPM SURVIVAL SIMULATOR",font="big")
+title=termcolor.colored(title,color="green")
+print(title)
+print("Welcome to SPM Survival Simulator! you will be experiencing the life of a SPM student and with some challenges and miracle adventures!")
+print("----------------------------------------------------------------------------------------------------------------------------------------")
+
+#creating menu 
+coloredMenu=termcolor.colored("MENU",color="blue")
+coloredMenu=pyfiglet.figlet_format("MENU",font="isometric1")
+print(coloredMenu)
+
+def menu():
+    print("[1] Start Game")
+    print("[2]View History")
+    print("[3] Exit Game\n")
+
+menu()
+option=int(input("Please select an option: "))
+while option !=3:
+    if option==1:
+       option1()
+      
+    elif option==2:
+     with open("playerdata.txt","r") as f:
+        content=f.read()
+        print(content)
+    
+    elif option<1 or option>3:
+        print("Invalid Option!")
+         
+    else:
+        break
+    
+    print()
+    menu()
+    option=int(input("Please select an option: "))
+print("Thank you for joining the game!")
+
+#game starting...
+def opyion1():
+    name=str(input("Enter your name; "))
+    gender=str(input("enter your gender(M/F): "))
+     #setting number of attempts
+    attempts=1 
+    with open("playerdata.txt","a") as f:
+         f.write("Player Name: ",name,"\n")
+         f.write("Player Gender:",gender.capitalize(),"\n")
+         f.write("Number of Attempts:", int(attempts),"\n")
+
+
